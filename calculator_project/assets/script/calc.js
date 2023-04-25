bc69ce44bd7983b46e6948c912a942ee203af4f6
JLStormSmith <jlsmith15@ualr.edu>
2023-04-25 17:48:19 +0000

    added links to some of the javascript assignments

--- calculator_project/assets/script/calc.js
+++ calculator_project/assets/script/calc.js
@@ -43,17 +43,16 @@ function calculate() {
 
 function clearForm(){
 /* Clears the whole screen and sets all the boxes to blank */
-document.getElementById("oper1").value = "";
-document.getElementById("Operand2").value = "";
-document.getElementById("oper2").value = "";
-document.getElementById("oper1").innerHTML = "";
-document.getElementById("oper2").innerHTML = "";
-document.getElementById("Operand1").innerHTML = "";
-document.getElementById("Operand2Error").innerHTML = "";
-document.getElementById("rdMin").innterHTML=""
-document.getElementById("AvgOperator").checked = false;
-document.getElementById("OperatorError").innerHTML = "";
-document.getElementById("Result").innerHTML = "";
+document.getElementById("Operand1").value = "";
+    document.getElementById("Operand1Msg").innerHTML = "";
+    document.getElementById("AddOperator").checked = false;
+    document.getElementById("SubtractOperator").checked = false;
+    document.getElementById("MultiplyOperator").checked = false;
+    document.getElementById("DivideOperator").checked = false;
+    document.getElementById("OperatorMsg").innerHTML = "";
+    document.getElementById("Operand2").value = "";
+    document.getElementById("Operand2Msg").innerHTML = "";
+    document.getElementById("Result").innerHTML = "";
 }
 /* This should validate the form */
 $( "#RadioExampleValidationCheckForm" ).validate({
