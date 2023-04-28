5a6b5f71131e5b44b084b4eaccfa8f41699ffe48
JLStormSmith <jlsmith15@ualr.edu>
2023-04-28 00:41:31 +0000

    Finishing the last of the js projects before the deadline

--- calculator_project/assets/script/calc.js
+++ calculator_project/assets/script/calc.js
@@ -1,62 +1,63 @@
 
 function calculate() {
-    if ($("#calcform").valid()){
+    if ($( "#myform" ).valid()){
 
         //gets the operands
         var operand1 = document.getElementById("Operand1").value;
         var operand2 = document.getElementById("Operand2").value;
+
+        
         
         var operand1fp = parseFloat (operand1);
         var operand2fp = parseFloat (operand2);
         
         var operator;
         if (document.getElementById("AddOperator").checked) {
-            operator = document.gettElementById("AddOperator").value;
+            operator = document.getElementById("AddOperator").value;
         }
         if (document.getElementById("SubtractOperator").checked) {
-            operator = document.gettElementById("SubtractOperator").value;
+            operator = document.getElementById("SubtractOperator").value;
         }
         if (document.getElementById("MultiplyOperator").checked) {
-            operator = document.gettElementById("MultiplyOperator").value;
+            operator = document.getElementById("MultiplyOperator").value;
         }
         if (document.getElementById("DivideOperator").checked) {
-            operator = document.gettElementById("DivideOperator").value;
+            operator = document.getElementById("DivideOperator").value;
         }
         var result;
 //calculate the operators
-        if (operator == "+"){
-           result = operand1fp + operand2fp;
-
-        }
-        if (operator == "-"){
-           result = operand1fp - operand2fp;
-
-        }
-        if (operator == "*"){
-           result = operand1fp * operand2fp;
-
-        }
-        if (operator == "/"){
-            result = operand1fp / operand2fp;
-
-        }
-        document.getElementById("Result").innterHTML = result.toString();
+       if(operator == "Add"){
+        result = operand1fp + operand2fp;
+       }
+       if(operator == "Sub"){
+        result = operand1fp - operand2fp;
+       }
+       if(operator == "Mult"){
+        result = operand1fp * operand2fp;
+       }
+        document.getElementById("Result").innerHTML= result.toString();
+        
 
 
-}
+    }
 }
 
 function clearForm(){
 /* Clears the whole screen and sets all the boxes to blank */
 document.getElementById("Operand1").value = "";
+document.getElementById("Operand2").value = "";
+
     document.getElementById("Operand1Msg").innerHTML = "";
+    document.getElementById("Operand2Msg").innerHTML = "";
+
     document.getElementById("AddOperator").checked = false;
     document.getElementById("SubtractOperator").checked = false;
     document.getElementById("MultiplyOperator").checked = false;
     document.getElementById("DivideOperator").checked = false;
     document.getElementById("OperatorMsg").innerHTML = "";
-    document.getElementById("Operand2").value = "";
-    document.getElementById("Operand2Msg").innerHTML = "";
+    
+
+ 
     document.getElementById("Result").innerHTML = "";
 }
 /* This should validate the form */
--- mars_rover_project_explorer/asset/mars.js
+++ mars_rover_project_explorer/asset/mars.js
@@ -1,5 +1,6 @@
-for (i = 0; i < 25; i++) {
-    // Note how we construct the name for image1, image2, etc...this sets the image source
-    document.getElementById("image" + i).src = msg.photos[i].img_src;
-    do  = msg.photos[i].camera.full_name;
+async function search(){
+    let Rover = $('input[name="Rover"]:checked').val();
+    let date = $('#Date').val();
+
+    makeAPICall(Rover, date);
 }
